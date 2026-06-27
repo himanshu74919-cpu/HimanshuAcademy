06e3e55642b859b23d74258853fd760c3126f25a
Himanshu <himanshu74919@gmail.com>
2026-06-27 23:15:32 +0530

    Added Featured Courses section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import FeaturedCourses from "../components/FeaturedCourses";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedCourses />
     </>
   );
 }
